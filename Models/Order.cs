06c4f26598d475a6f62c17284ef5c1260ffb0ba6
Amna Riaz <amnariaz94@outlook.com>
2025-03-20 00:26:32 +0500

    Created Order Model with properties for OrderAmount, CustomerType, Discount, and FinalAmount. Implemented CalculateDiscount method to calculate discount based on customer type.

---
+++ Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderFlow.Models
+{
+    // Order properties
+    public class Order
+    {
+        [Required]
+        public decimal OrderAmount { get; set; }
+
+        [Required]
+        public bool IsCustomerLoyal { get; set; }
+
+        public decimal OrderDiscount { get; set; }
+        public decimal FinalAmount { get; set; }
+
+        // Order methods
+        public void CalculateDiscount()
+        {
+            if (OrderAmount >= 100 && IsCustomerLoyal == true)
+            {
+                OrderDiscount = OrderAmount * 0.10m;
+            }
+            else
+            {
+                OrderDiscount = 0;
+            }
+            FinalAmount = OrderAmount - OrderDiscount;
+
+        }
+
+
+
+    }
+}
