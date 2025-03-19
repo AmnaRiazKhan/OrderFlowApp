17909f7eed67564cc6749ea8a86d609ea3a9652c
Amna Riaz <amnariaz94@outlook.com>
2025-03-20 00:18:03 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrderFlowApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
